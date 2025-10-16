d1a75156127c36a02873841b91dcea2e6ed2a7fd
AaronVelasco93 <aaronvelasco1993@outlook.com>
2025-10-16 16:31:26 -0600

    Uso de accion php

---
+++ process.php
@@ -0,0 +1,9 @@
+<?php
+$nombreEvento=$_POST['event_name'];
+$fechaEvento=$_POST['event_date'];
+$numeroCuenta=$_POST[''];
+echo $nombreEvento;
+echo $fechaEvento;
+
+header ('Locatio: ./index.html');
+?>
