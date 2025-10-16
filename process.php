b585fd8583e7ecfe86cc4b4d4c1f320fc5a37c65
Cristian Arias Pineda <ariascristian2207@gmail.com>
2025-10-16 16:51:40 -0600

    Add validations

--- process.php
+++ process.php
@@ -1,10 +1,15 @@
 <?php
-$nombreEvento=$_POST['event_name'];
-$fechaEvento=$_POST['event_date'];
-$numeroCuenta=$_POST[''];
+$nombreEvento = $_POST['event_name'];
+$fechaEvento = $_POST['event_date'];
+$numeroCuenta = $_POST['account_number'];
+
+$nombreEvento = htmlspecialchars($nombreEvento);
+$fechaEvento = htmlspecialchars($fechaEvento);
+$numeroCuenta = htmlspecialchars($numeroCuenta);
+
 echo $nombreEvento;
 echo $fechaEvento;
 echo $numeroCuenta;
 
-header ('Location: ./index.html');
-?>
+header('Location: ./index.html');
+exit();
